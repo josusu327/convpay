682c15945f1969a4e5cfd83d0e592b4939040904
조수빈 <whtnqls03@gmail.com>
2025-01-14 15:18:49 +0900

    250114 3th commit. not Spring

--- src/main/java/com/zerobase/convpay/UserClient.java
+++ src/main/java/com/zerobase/convpay/UserClient.java
@@ -1,5 +1,6 @@
 package com.zerobase.convpay;
 
+import com.zerobase.convpay.config.ApplicationConfig;
 import com.zerobase.convpay.dto.PayCancelRequest;
 import com.zerobase.convpay.dto.PayCancelResponse;
 import com.zerobase.convpay.dto.PayRequest;
@@ -11,16 +12,20 @@ import com.zerobase.convpay.type.PayMethodType;
 public class UserClient {
     public static void main(String[] args) {
         // '사용자' -> 편결이 -> 머니
-        ConveniencePayService conveniencePayService = new ConveniencePayService();
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+//        ConveniencePayService conveniencePayService = new ConveniencePayService();
+        ConveniencePayService conveniencePayService = applicationConfig.conveniencePayServiceDiscountPayMethod();
 
         // G25, 결제 1000원
-        PayRequest payRequest = new PayRequest(PayMethodType.CARD, ConvenienceType.G25, 1000);
+        PayRequest payRequest = new PayRequest(PayMethodType.CARD,
+                ConvenienceType.G25, 50);
         PayResponse payResponse = conveniencePayService.pay(payRequest);
 
         System.out.println(payResponse);
 
         // G25, 취소 500원
-        PayCancelRequest payCancelRequest = new PayCancelRequest(PayMethodType.MONEY, ConvenienceType.G25, 500);
+        PayCancelRequest payCancelRequest = new PayCancelRequest(PayMethodType.MONEY,
+                ConvenienceType.G25, 500);
         PayCancelResponse payCancelResponse = conveniencePayService.payCancel(payCancelRequest);
 
         System.out.println(payCancelResponse);
